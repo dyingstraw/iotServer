e29e6277462f66a016503c8dee06f2b69470b0f5
dyingstraw <1083608577@qq.com>
2019-06-29 22:10:57 +0800

    获取配置文件的配置类

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+/**
+ * @program: netty_study
+ * @description:
+ * @author: dyingstraw
+ * @create: 2019-06-10 11:37
+ **/
+public class AppConfig {
+    public static String getConfigByName(String name) throws IOException {
+        // Properties properties = new Properties();
+        // properties.load(new FileReader("app.properties"));
+        return "";
+    }
+}
